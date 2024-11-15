742d4e5fc13702107675b85acaaa879008361c8d
Mike Frank <mcfrank@stanford.edu>
2024-11-15 13:18:33 -0800

    paper

--- helper/common.R
+++ helper/common.R
@@ -19,11 +19,17 @@ knitr::opts_chunk$set(cache.extra = knitr::rand_seed, cache = TRUE,
 options(dplyr.summarise.inform = FALSE)
 
 
-.font <- "Source Sans Pro"
-theme_set(theme_bw(base_size = 14, base_family = .font))
+# .font <- "Source Sans Pro"
+# theme_set(theme_bw(base_size = 14, base_family = .font))
+theme_set(theme_bw(base_size = 10))
 theme_update(panel.grid = element_blank(),
              strip.background = element_blank(),
              legend.key = element_blank(),
              panel.border = element_blank(),
              axis.line = element_line(),
              strip.text = element_text(face = "bold"))
+
+# helper function for scaling to the variance of a particular age group
+age_scale <- function (x, age, min = 16, max = 20) {
+  (x - mean(x, na.rm=TRUE)) / sd(x[age >= min & age <= max], na.rm=TRUE)
+}
